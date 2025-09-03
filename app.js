95af31e4e18aed6168c3d1c6b2e79fac61b9b914
Dano0724 <danlorenzana0724@gmail.com>
2025-09-03 09:55:46 -0600

    Se crea funcion para añadir amigos a la lista y alert en caso de que el campo este vacío

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Se crea un array para almacenas los nombres
+let amigos = [];
+
+// Función para agregar un amigo 
+function agregarAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim(); 
+
+    
+    if (nombreAmigo === "" ) {
+        alert("Por favor, inserte un nombre válido (solo letras)");
+        return;
+    } 
+
+   
+
+  
+    amigos.push(nombreAmigo);
+
+
+    inputAmigo.value = "";
+
+    
+    actualizarLista();
+}
