0ec6319951397631f3dbf58100d77da12bb607f2
Dano0724 <danlorenzana0724@gmail.com>
2025-09-03 23:16:21 -0600

    Se añade la funcion de sortear al amigo secreto

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function agregarAmigo() {
 
     
     if (nombreAmigo === "" ) {
-        alert("Por favor, inserte un nombre válido (solo letras)");
+        alert("Por favor, inserte un nombre.");
         return;
     } 
 
@@ -33,7 +33,7 @@ function actualizarLista() {
 
     listaAmigos.innerHTML = ""; 
 
-    // Agregar cada amigo a la lista
+    // Agregar nombre de Amigo a la lista
     amigos.forEach(amigo => {
         const li = document.createElement("li");
         li.textContent = amigo;
@@ -41,5 +41,26 @@ function actualizarLista() {
     });
 
 }
+// Función para el sorteo
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
 
-console.log ("listaAmigos");
+    // Elegir amigo al azahar   
+    const amigoAleatorio = amigos[Math.floor(Math.random() * amigos.length)];
+
+    // Seleccionar la lista de resultados
+    const listaResultado = document.getElementById("resultado");
+
+    // Limpiar la lista
+    listaResultado.innerHTML = "";
+
+    // Crear un <li> con el nombre sorteado
+    const li = document.createElement("li");
+    li.textContent = `Tu amigo secreto es: ${amigoAleatorio}`;
+
+    // Agregarlo a la lista
+    listaResultado.appendChild(li);
+}
